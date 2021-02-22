055a41b759e0ade3879d919605bc95ab7ed47d8a
dam109 <dam109@gmail.com>
2021-02-22 11:26:08 +0100

    implementacion test testGetDni

--- src/ed/examen/modelo/CursoTest.java
+++ src/ed/examen/modelo/CursoTest.java
@@ -6,34 +6,57 @@ import org.junit.jupiter.api.Test;
 
 class CursoTest {
 
-	Curso pepe=new Curso();
+	Curso c1=new Curso();
 	Persona alex=new Persona("18087364A", "alex", "merino");
+	Persona pepe=new Persona("18087", "pepe", "dfh");
+	
 	
 	@Test
 	void testEliminarAlumno() {
-		fail("Not yet implemented");
+		c1.aniadirAlumno(alex);
+			int inicial=c1.numeroAlumnos();
+			try {
+				c1.eliminarAlumno(alex.getDni());
+			} catch (Exception e) {
+				
+			}
+			int ultimo=c1.numeroAlumnos();
+			assertEquals(inicial, ultimo+1);
+			
+		//clase invalida	
+			c1.aniadirAlumno(pepe);
+			int antes=c1.numeroAlumnos();
+			try {
+				c1.eliminarAlumno(pepe.getDni());
+			} catch (Exception e) {
+				
+			}
+			int despues=c1.numeroAlumnos();
+			assertEquals(antes, despues+1);
 	}
 
 	@Test
 	void testAniadirAlumno() {
-		int inicial=pepe.numeroAlumnos();
-		pepe.aniadirAlumno(alex);
-		int ultimo=pepe.numeroAlumnos();
+		
+		int inicial=c1.numeroAlumnos();
+		c1.aniadirAlumno(alex);
+		int ultimo=c1.numeroAlumnos();
 		assertEquals(inicial, ultimo-1);
 	}
 
 	@Test
 	void testEstaRegistrado() {
-		fail("Not yet implemented");
+		c1.estaRegistrado(alex.getDni());
 	}
 
 	@Test
 	void testCurso() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testNumeroAlumnos() {
-		fail("Not yet implemented");
+		int longitud=c1.numeroAlumnos();
+		assertEquals(longitud, 0);
 	}
 }
---
+++ src/ed/examen/modelo/PersonaTest.java
@@ -0,0 +1,60 @@
+package ed.examen.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+	
+	Persona p=new Persona("18087364S", "Paco", "pico");
+
+
+	@Test
+	void testGetDni() {
+		assertEquals("18087364S", p.getDni());
+	}
+
+	@Test
+	void testSetDni() {
+		try {
+			p.setDni("12345678W");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals("12345678W", p.getDni());
+		
+		//clase invalida
+		boolean excepcionlanzada=false;
+		try {
+			p.setDni("12345678");
+			
+		}catch (Exception e) {
+			excepcionlanzada=true;
+		}
+		assertTrue(excepcionlanzada);
+	}
+
+	@Test
+	void testGetNombre() {
+		assertEquals("Paco", p.getNombre());
+	}
+
+	@Test
+	void testSetNombre() {
+		p.setNombre("Pedro");
+		assertEquals("Pedro", p.getNombre());
+	}
+
+	@Test
+	void testGetApellido1() {
+		assertEquals("pico", p.getApellido1());
+	}
+
+	@Test
+	void testSetApellido1() {
+		p.setApellido1("Navarro");
+		assertEquals("Navarro", p.getApellido1());
+	}
+
+
+}
